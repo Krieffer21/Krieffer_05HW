32b7f5192adc79c64a196cbf45f9c710d1dc78d9
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-04-29 15:18:59 -0700

    time block function apeended classes to html

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,36 @@
 
 $(document).ready(function() {
-    
+    hours = ["9am", "10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
 // var today = new date(moment().subtract(10, 'days').calendar());
 //     $("#currentday").append(today);
 
-function timeBlocks() {
-for (let i = 0; i < 8; i++) {
-    var timeBlock = $("<div>");
-    $(".container").append(timeBlock);
-    timeBlock.attr("class", "time-block");
-}
+    function timeBlocks() {
+        for (let i = 0; i < 8; i++) {
+            var timeBlock = $("<div>");
+            $(".container").append(timeBlock);
+            timeBlock.attr("class", "time-block");
+
+            var hour = $("<div>");
+            timeBlock.append(hour);
+            hour.attr("class","hour");
+            hour.text(hours[i]);
+            
+            var row = $("<div>");
+            timeBlock.append(row);
+            row.attr("class","row");
+            
+            var textArea = $("<textarea>");
+            row.append(textArea);
+            textArea.attr("id","text" + i);
+
+            var saveBtn = $("<button>");
+            timeBlock.append(saveBtn);
+            saveBtn.attr("class",".saveBtn");
+            saveBtn.attr("id", "save" + i); 
+        }
     }
+
     timeBlocks();
-
-// function textBox() {
-//     var textarea = "";
-//     var tasks = document.createElement("article");
-//     $("textarea").on("click", function() {
-//         // append text, empty array to that article
-//         $("tasks").text(textarea);
-
-//     });
-// }
-
-// var saveBtn = document.createElement(".saveBtn");
-//     $(".saveBtn").append(body);
 
 
 });
