ec9bb7eefe7655c40f8eea2f4c2de28d2dda52d5
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-04-30 12:59:27 -0700

    save to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,45 @@
 
 $(document).ready(function() {
     hours = ["9am", "10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
-// var today = new date(moment().subtract(10, 'days').calendar());
-//     $("#currentday").append(today);
+
+    // var today = todate(moment().format("MMM Do YY"));
+    // today.attr("id","currentDay");
 
     function timeBlocks() {
         for (let i = 0; i < 8; i++) {
             var timeBlock = $("<div>");
             $(".container").append(timeBlock);
-            timeBlock.attr("class", "time-block");
+            timeBlock.attr("class", "row time-block");
 
             var hour = $("<div>");
             timeBlock.append(hour);
             hour.attr("class","hour");
             hour.text(hours[i]);
             
-            var row = $("<div>");
-            timeBlock.append(row);
-            row.attr("class","row");
-            
             var textArea = $("<textarea>");
-            row.append(textArea);
+            timeBlock.append(textArea);
             textArea.attr("id","text" + i);
+            textArea.attr("class","description");
 
             var saveBtn = $("<button>");
             timeBlock.append(saveBtn);
-            saveBtn.attr("class",".saveBtn");
+            saveBtn.attr("class","saveBtn");
             saveBtn.attr("id", "save" + i); 
         }
     }
 
     timeBlocks();
 
+    for (let i = 0; i < 8; i++) {
+        var store = localStorage.getItem("save" + i);
+        $("#text" + i).text(store);
+    }
+
+    $(".saveBtn").on("click",function(event) {
+    var element = event.target.id;
+    var match = element.charAt(element.length -1);
+    var textContent = $("#text" + match).val();
+    localStorage.setItem(element, textContent);
+});
 
 });
