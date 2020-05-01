4bdf0bd414b9dcaf59b2a5893cc5cf1d76b27da1
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-05-01 15:31:29 -0700

    added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,22 @@
 
 $(document).ready(function() {
+    // Hours that need to be displayed
     hours = ["9am", "10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
 
+    // Prints current date
     var today = moment().format("MMMM Do YYYY");
     $("#currentDay").append(today);
 
+    // tracks what the hoir is in millitary time
     var time = moment().format("H");
+        // logged to show it finds the current hour
         console.log(time);
+    // Making the time into an integer
     var timeInt = parseInt(time) -9;
 
+    //Appends the time blocks to the container.
     function timeBlocks() {
+        // Appends for every hour in the hours array.
         for (let i = 0; i < 9; i++) {
 
             var timeBlock = $("<div>");
@@ -25,8 +32,7 @@ $(document).ready(function() {
             timeBlock.append(textArea);
             textArea.attr("id","text" + i);
 
-            // if i< timeInt then its the past so add class past using attr. or do while loop?
-            
+            //Using the given css class color code the text area based on what time it is.  
             if (i < timeInt) { 
             textArea.attr("class", "description past");
             } else if (i > timeInt) {
@@ -43,12 +49,12 @@ $(document).ready(function() {
     }
     timeBlocks();
     
-
+    // Storing text that was saved to local storage
     for (let i = 0; i < 8; i++) {
         var store = localStorage.getItem("save" + i);
         $("#text" + i).text(store);
     }
-
+    // Saving text when the save button is clicked
     $(".saveBtn").on("click",function(event) {
     var element = event.target.id;
     var match = element.charAt(element.length -1);
